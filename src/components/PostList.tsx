3d3c3a5727c3cd472119f3f89ea5e39dd0886918
agcudco <agcudco@espe.edu.ec>
2025-06-20 07:35:39 -0500

    correcion

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { Button } from 'primereact/button';
 
 interface Post {
     id: number;
